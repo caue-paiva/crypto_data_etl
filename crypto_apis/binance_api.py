0bc94f15b004d7b9df0bab859bf50547caa9b4bd
“Caue <cauepaivalira@outlook.com>
2024-01-22 18:47:18 -0300

    more functions to manipulate dataframes

--- crypto_apis/binance_api.py
+++ crypto_apis/binance_api.py
@@ -4,7 +4,7 @@ import json
 BTCUSDT, ETHUSDT, SOLUSDT...
 """
 
-def get_binance_server_time()->int | None:
+def binance_server_time()->int | None:
         response = requests.get("https://api.binance.com/api/v3/time")
         if response.status_code == 200:
              json_result:dict = response.json()
@@ -25,16 +25,34 @@ def op_is_sell(transaction:dict)->bool | None:
 def min_to_ms(min:int | float)->int:
      return int(min*60*1000)
 
-def get_binance_trading_volume( time_window_min: float | int ,end_unix_time:int = 0 ,crypto_token:str = "BTCUSDT")->dict[str,float|int]:
+def binance_crypto_price(symbol:str, end_time_unix:int)->float | None:
+     UNIX_TIME_INTERVAL = 1000 #in ms, approx 10s
+     kline_data_url = "https://api.binance.com/api/v3/klines"
+     params:dict = {
+          "symbol": symbol,
+          "interval": "1s",
+          "startTime": end_time_unix - UNIX_TIME_INTERVAL,
+          "endTime" :  end_time_unix,
+          "limit": 1
+     }
+     response = requests.get(url=kline_data_url, params=params)
+     if response.status_code == 200:
+            json_result:list[list[str | float]] = response.json()
+     else:
+            print(F"Failure in getting aggregate trading data: {response.status_code}, response: {response.text}")
+            return None
+     
+     return float(json_result[0][1])
+
+def binance_trading_volume( time_window_min: float|int,end_unix_time:int = 0,crypto_token:str = "BTCUSDT")->dict[str,float|int|None]:
    
     MAX_REQUEST_TIMEFRAME = 60000 #90k Ms is the limit time (based on some test) where you can get all aggregata trading without hitting the 1000 results limit on the binance API
     time_window_ms = min_to_ms(time_window_min)
 
     requests_needed:int = math.ceil(time_window_ms/MAX_REQUEST_TIMEFRAME)
-    print(requests_needed)
 
     if end_unix_time == 0:
-        end_time: int | None = get_binance_server_time()
+        end_time: int | None = binance_server_time()
         if end_time ==  None:
             raise Exception("Initial time for the binance server wasnt able to be established")
     else:
@@ -42,6 +60,8 @@ def get_binance_trading_volume( time_window_min: float | int ,end_unix_time:int 
     
     total_transactions:int = 0
     requests_hitting_limit:int = 0 #number of requests hitting the 1000 responses API limit
+   
+    initial_price: float | None = binance_crypto_price(symbol=crypto_token,end_time_unix=end_time)
 
     total_sell_coins: float = 0.0
     total_sell_usd:float  = 0.0
@@ -56,8 +76,7 @@ def get_binance_trading_volume( time_window_min: float | int ,end_unix_time:int 
             "endTime":    end_time,
             "limit" : 1000
         }
-        #print(end_time)
-
+        
         response= requests.get("https://api.binance.com/api/v3/aggTrades", params=params)
         if response.status_code == 200:
                 json_result:list[dict] = response.json()
@@ -83,24 +102,29 @@ def get_binance_trading_volume( time_window_min: float | int ,end_unix_time:int 
 
         end_time -= MAX_REQUEST_TIMEFRAME
 
-    print(f"requests hitting limit: {requests_hitting_limit}")
+    final_price: float | None = binance_crypto_price(symbol=crypto_token,end_time_unix=end_time)
     
     if requests_hitting_limit/requests_needed > 0.5:
-          raise Warning("More than half of requests are hitting the binance API rate limit")
+          print("More than half of requests are hitting the binance API rate limit")
     
     return {
-        
-            f"{crypto_token}_COINS_SOLD": total_sell_coins,
-            f"{crypto_token}_USD_SOLD": total_sell_usd,
-            f"{crypto_token}_COINS_BOUGHT": total_buy_coins,
-            f"{crypto_token}_USD_BOUGHT": total_buy_usd,
-            f"{crypto_token}_NET_FLOW": total_buy_usd - total_sell_usd,
-            f"{crypto_token}_TOTAL_TRANSACTIONS": total_transactions
+            f"{crypto_token}_INITIAL_PRICE": initial_price ,
+            f"{crypto_token}_FINAL_PRICE": final_price, 
+            f"{crypto_token}_COINS_SOLD": round(total_sell_coins,3),
+            f"{crypto_token}_USD_SOLD": round(total_sell_usd,3),
+            f"{crypto_token}_COINS_BOUGHT": round(total_buy_coins,3),
+            f"{crypto_token}_USD_BOUGHT": round(total_buy_usd,3),
+            f"{crypto_token}_NET_FLOW": round(total_buy_usd - total_sell_usd,3),
+            f"{crypto_token}_TOTAL_AGGRT_TRANSACTIONS": total_transactions
     }
    
-if __name__ == "main":
-   print(get_binance_trading_volume(30, crypto_token="SOLUSDT"))
-
+if __name__ == "__main__":
+   #print(binance_trading_volume(30, crypto_token="SOLUSDT"))
+   cur_time = binance_server_time()
+   return_data = binance_crypto_price("BTCUSDT",cur_time) # type: ignore
+   print(return_data)
+  
+#binance_kline_data("BTCUSDT")
 #print(get_binance_server_time())
 #print(json_result)
 #print(len(json_result))
--- process_cripto_data.py
+++ process_cripto_data.py
@@ -1,75 +1,115 @@
-from crypto_apis.binance_api import get_binance_trading_volume , get_binance_server_time, min_to_ms
-import requests , math
+from crypto_apis.binance_api import binance_trading_volume , binance_server_time, min_to_ms
+import math
 from datetime import datetime , timedelta
 import pandas as pd
 """
+Try to use Pyarrow backend for pandas for this project
 
+On avg 80 Bytes per row of the dataframe
 
+with 100mb we could have 970903 rows, each row being 10mins, giving us 6742 days of data or 18 years
 
+If the window was lowered t0 5 min you could have 9 years of data with 100mb per CSV file 
+
+If the window is 2.5min or 150s you could have 4.5 years of data or 5.5 years with 120mb (best option)
 
 
 """
 
+def add_crypto_dataframes(newer_data:pd.DataFrame , older_data: pd.DataFrame)->pd.DataFrame:
+    """
+    The lower the index the more recent the data is
 
+    Dates on the newer data df need to be more recent than on the older data
 
-def crypto_data_to_df(time_frame_hours:int | float, start_time_unix:int = 0)-> pd.DataFrame: 
-    time_frame_hours = time_frame_hours * 60
-    CRYPTO_CURRUNCIES = ["BTC"] #,"ETH","SOL"
-    DATA_TIME_WINDOW_MIN = 30 #how many minutes of data does each column represent
-    num_columns = math.ceil(time_frame_hours/DATA_TIME_WINDOW_MIN)
-
-    columns_list:list[str] = []
-    for cur in CRYPTO_CURRUNCIES:
-        columns_list.extend(
-             [
-                 f"{cur}_COINS_BOUGHT",
-                 f"{cur}_COINS_SOLD",
-                 f"{cur}_NET_FLOW",
-                 f"{cur}_TOTAL_TRANSACTIONS"
-             ]
-        )
-    df = pd.DataFrame(columns = columns_list)
-    print(df)
-    #df = df.set_axis(axis=1, labels=columns_list)
+    No data is supposed to be lost on this function
     
-    start_date = datetime.now()
-    cur_unix_time: int | None = get_binance_server_time()
+    """
+    if not isinstance(newer_data, pd.DataFrame) or not isinstance( older_data, pd.DataFrame):
+        raise TypeError("input data isnt a pandas Dataframe")
+
+    newer_date = newer_data.at[0,"DATE"]
+    old_date = older_data.at[0,"DATE"]
+    print(newer_date,old_date)
+    if old_date > newer_date:
+        raise IOError("most recent date from the older dataframe is more recent than the dates from the newer dataframe")
+
+    return pd.concat(objs=[newer_data,older_data], axis= 0, ignore_index=True)
+
+def update_crypto_dataframes(newer_data:pd.DataFrame , older_data: pd.DataFrame, max_row_num: int = 1000000)->pd.DataFrame:
+    """
+    This function will keep the max size of the current older_data df and remove its older indexes 
+    while adding newer data to its top
+    
+    """
+    if not isinstance(newer_data, pd.DataFrame) or not isinstance( older_data, pd.DataFrame):
+        raise TypeError("input data isnt a pandas Dataframe")
+
+
+    older_data_row_num:int = older_data.shape[0]  
+    newer_data_row_num:int = newer_data.shape[0]
+
+    if older_data_row_num + newer_data_row_num <= max_row_num:
+        raise IOError("The new size after apending the dataframes doesnt cross the limit, please use the `add_crypto_dataframes` function instead  ")
+    else:
+        rows_to_remove: int = (older_data_row_num + newer_data_row_num) - max_row_num
+    
+    first_row_to_remove:int = older_data_row_num - rows_to_remove
+    older_data = older_data.drop(index=[i for i in range(first_row_to_remove, older_data_row_num)])
+
+    return pd.concat(objs=[newer_data,older_data], axis= 0, ignore_index=True)
+
+def crypto_data_to_df(time_frame_hours:int|float, crypto_token: str)-> pd.DataFrame: 
+    
+    if not isinstance(time_frame_hours,int) and not isinstance(time_frame_hours,float):
+        raise TypeError("Input time frame isnt an int or float")
+    
+    time_frame_mins = time_frame_hours * 60
+    DATA_TIME_WINDOW_MIN = 5 #how many minutes of data does each column represent
+    num_rows = math.ceil(time_frame_mins/DATA_TIME_WINDOW_MIN) #how many time_window rows will be needed to cover the entire time_frame passed as arg
+
+    columns_list:list[str] = [   
+                  "DATE", 
+                 f"{crypto_token}_INITIAL_PRICE",
+                 f"{crypto_token}_FINAL_PRICE",
+                 f"{crypto_token}_COINS_BOUGHT",
+                 f"{crypto_token}_COINS_SOLD",
+                 f"{crypto_token}_NET_FLOW",
+                 f"{crypto_token}_TOTAL_AGGRT_TRANSACTIONS"
+                            ]
+    
+    df = pd.DataFrame(columns = columns_list)
+    
+    start_date = datetime.now().replace(microsecond=0)
+    cur_unix_time: int | None = binance_server_time()
 
     if cur_unix_time == None:
        raise Exception("Initial time for the binance server wasnt able to be established")
 
-    for i in range(num_columns): #time window loop
-        print(i)
+    for i in range(num_rows): #time window loop
 
-        currency_data:list[float|int] = []
-        for cur in CRYPTO_CURRUNCIES: #currency loop 
-            cur += "USDT"
-            
-            data:dict = get_binance_trading_volume(
+        cur = crypto_token + "USDT" #binance symbol for token to USDT (TETHER) price   
+        data:dict = binance_trading_volume(
                     time_window_min=DATA_TIME_WINDOW_MIN,
                     end_unix_time= cur_unix_time,
                     crypto_token= cur
-            )
-            currency_data.extend(
-                [
-                data.get(f"{cur}_COINS_BOUGHT",-1),
-                data.get(f"{cur}_COINS_SOLD",-1),
-                data.get(f"{cur}_NET_FLOW",-1),
-                data.get(f"{cur}_TOTAL_TRANSACTIONS",-1)
-                ]
-            )
-            
-            
+                   )
+        currency_data:list[float|int|None|datetime] = [
+                        start_date, 
+                        data.get(f"{cur}_INITIAL_PRICE",None),
+                        data.get(f"{cur}_FINAL_PRICE",None),
+                        data.get(f"{cur}_COINS_BOUGHT",None),
+                        data.get(f"{cur}_COINS_SOLD",None),
+                        data.get(f"{cur}_NET_FLOW",None),
+                        data.get(f"{cur}_TOTAL_AGGRT_TRANSACTIONS",None)
+                    ]
+        
         cur_unix_time  -= min_to_ms(DATA_TIME_WINDOW_MIN)
-        df.loc[start_date] = currency_data # type: ignore
-        start_date += timedelta(minutes=DATA_TIME_WINDOW_MIN)
+        df.loc[i] = currency_data # type: ignore
+        start_date -= timedelta(minutes=DATA_TIME_WINDOW_MIN)
     
-    
-    print(df)
-
     return df
 
-   # get_binance_trading_volume()
-    #pass
 
-crypto_data_to_df(1)
+#df: pd.DataFrame = crypto_data_to_df(0.5, "BTC" )
+#df.to_csv("teste2.csv", index= False)
